24a62c2bc1ac772d2cf902d2a260ca096c3e4e8a
Rhuan Kowic <dev.rhuankowic@gmail.com>
2026-06-09 20:40:25 -0300

    feat(routes): cria rota de teste para conexao com o banco

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import { pool } from "./db/database.js";
 
 const server = express();
+const PORT = process.env.PORT;
 
 server.get("/", function (req, res) {
   res.json({ message: "IT Resource Manager" });
 });
 
-server.listen(5500, function () {
-  console.log("Servidor rodando: http://localhost:5500/");
+server.get("/enterprises", async function (req, res) {
+  try {
+    const result = await pool.query("SELECT * FROM enterprise");
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+server.listen(PORT, function () {
+  console.log(`Servidor rodando: http://localhost:${PORT}/`);
 });
